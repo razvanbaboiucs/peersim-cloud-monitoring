0aab35a264892b4f5e7275b00952d7de442d0403
razvanbaboiucs <razvanbaboiu@gmail.com>
2025-05-29 18:51:58 +0300

    Fix anomaly control

--- src/main/java/com/razvanbaboiu/cloudmonitoring/control/AnomalyControl.java
+++ src/main/java/com/razvanbaboiu/cloudmonitoring/control/AnomalyControl.java
@@ -2,6 +2,7 @@ package com.razvanbaboiu.cloudmonitoring.control;
 
 
 import com.razvanbaboiu.cloudmonitoring.protocol.CloudServiceProtocol;
+import com.razvanbaboiu.cloudmonitoring.utils.NodeTypeRatio;
 import peersim.config.Configuration;
 import peersim.core.CommonState;
 import peersim.core.Control;
@@ -17,15 +18,16 @@ public class AnomalyControl implements Control {
 
     @Override
     public boolean execute() {
-        // Randomly spike metrics for 5% of CloudServiceNodes
-        for (int i = 0; i < Network.size(); i++) {
+        int totalNodes = Network.size();
+        int cloudServiceEnd = (int) (totalNodes * NodeTypeRatio.CLOUD_SERVICE_NODE_RATIO);
+        // Randomly spike metrics for 10% of CloudServiceNodes
+        for (int i = 0; i < cloudServiceEnd; i++) {
             Node node = Network.get(i);
+            CloudServiceProtocol csp = (CloudServiceProtocol) node.getProtocol(protocolID);
             if (CommonState.r.nextDouble() < 0.1) {
-                CloudServiceProtocol csp = (CloudServiceProtocol) node.getProtocol(protocolID);
                 csp.setCpu(90.0 + (CommonState.r.nextDouble() * 10)); // Spike to 100%
                 csp.setMemory(85.0 + (CommonState.r.nextDouble() * 15));
             } else {
-                CloudServiceProtocol csp = (CloudServiceProtocol) node.getProtocol(protocolID);
                 csp.setCpu(20.0 + (CommonState.r.nextDouble() * 30)); // 20-50% initial load
                 csp.setMemory(30.0 + (CommonState.r.nextDouble() * 40)); // 30-70% initial usage
             }
